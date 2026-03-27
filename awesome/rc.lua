593174053343820b8b0204d16cbd9eb5784af447
vicfred <git@vicfred.dev>
2026-03-26 21:04:23 -0600

    remove desktops from awesomewm

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -265,7 +265,7 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "壱", "弐", "参", "四", "五", "六", "七", "八", "九" }, s, awful.layout.layouts[1])
+    awful.tag({ "壱", "弐", "参", "四", "五" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
